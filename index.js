784865e3a66db0bfaa7b8156e164ada1f2a8f7da
Xie Yi <wez733@live.cn>
2019-01-07 06:44:55 +0800

    remove echo

--- index.js
+++ index.js
@@ -212,19 +212,6 @@ bot.onText(/\/stop/, (msg) => {
     bot.sendMessage(chatId, 'Stopped, chat ID: ' + chatId);
 });
 
-// Matches "/echo [whatever]"
-bot.onText(/\/echo (.+)/, (msg, match) => {
-    // 'msg' is the received Message from Telegram
-    // 'match' is the result of executing the regexp above on the text content
-    // of the message
-
-    const chatId = msg.chat.id;
-    const resp = match[1]; // the captured "whatever"
-
-    // send back the matched "whatever" to the chat
-    bot.sendMessage(chatId, resp);
-});
-
 // bot.on('sticker', (msg) => {
     // const chatId = msg.chat.id;
     // logger.info('[' + chatId + '] ' + msg.sticker.file_id);
